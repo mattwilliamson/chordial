8ebcf3c566ba78ccdc55e39d61d6a192c74c762b
Matt Williamson <mwilliamson@mwilliamson-vmware.(none)>
2009-02-03 11:27:48 -0500

    Added gen_chord.erl to the app file and added the gen_server behaviour to the gen_chord module.

--- lib/chordial/src/gen_chord.erl
+++ lib/chordial/src/gen_chord.erl
@@ -1,34 +1,128 @@
 %%%-------------------------------------------------------------------
-%%% @author Matt Williamson <mwilliamson@dawsdesign.com>
+%%% @author Matt Williamson <dawsdesign@gmail.com>
 %%% @copyright (C) 2009, Matt Williamson
-%%% @doc This module provides a behaviour for nodes implementing
-%%% the chord DHT algorithm. It provides an interface for callbacks
-%%% such as nodes joining or leaving the DHT. This allows the client
-%%% of chord to do things like start replication when a node joins
-%%% or start a failover process when one leaves.
+%%% @doc This module provides a chord server as well as callbacks so
+%%% the client using chord can be notified of events, such as a node
+%%% joining or leaving the ring.
 %%%
 %%% @end
-%%% Created : 30 Jan 2009 by Matt Williamson <mwilliamson@dawsdesign.com>
+%%% Created :  3 Feb 2009 by Matt Williamson <dawsdesign@gmail.com>
 %%%-------------------------------------------------------------------
 -module(gen_chord).
 
+-behaviour(gen_server).
+
 %% API
--export([behaviour_info/1]).
+-export([start_link/0]).
+
+%% gen_server callbacks
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2,
+	 terminate/2, code_change/3]).
+
+-define(SERVER, ?MODULE). 
+
+-record(state, {}).
 
 %%%===================================================================
 %%% API
 %%%===================================================================
 
 %%--------------------------------------------------------------------
-%% @doc Get list of callbacks for the chord behaviour.
-%% @spec behaviour_info(callbacks) -> [{CallbackFun, Arity}]
-%% where
-%%   CallbackFun = atom(),
-%%   Arity = integer()
+%% @doc
+%% Starts the server
+%%
+%% @spec start_link() -> {ok, Pid} | ignore | {error, Error}
 %% @end
 %%--------------------------------------------------------------------
-behaviour_info(callbacks) ->
-    [{new_predecessor, 1}, {new_successor, 1}, {get, 1}, {put, 2}].
+start_link() ->
+    gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+
+%%%===================================================================
+%%% gen_server callbacks
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Initiates the server
+%%
+%% @spec init(Args) -> {ok, State} |
+%%                     {ok, State, Timeout} |
+%%                     ignore |
+%%                     {stop, Reason}
+%% @end
+%%--------------------------------------------------------------------
+init([]) ->
+    {ok, #state{}}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling call messages
+%%
+%% @spec handle_call(Request, From, State) ->
+%%                                   {reply, Reply, State} |
+%%                                   {reply, Reply, State, Timeout} |
+%%                                   {noreply, State} |
+%%                                   {noreply, State, Timeout} |
+%%                                   {stop, Reason, Reply, State} |
+%%                                   {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
+handle_call(_Request, _From, State) ->
+    Reply = {error, unknown_call},
+    {reply, Reply, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling cast messages
+%%
+%% @spec handle_cast(Msg, State) -> {noreply, State} |
+%%                                  {noreply, State, Timeout} |
+%%                                  {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
+handle_cast(_Msg, State) ->
+    {noreply, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling all non call/cast messages
+%%
+%% @spec handle_info(Info, State) -> {noreply, State} |
+%%                                   {noreply, State, Timeout} |
+%%                                   {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% This function is called by a gen_server when it is about to
+%% terminate. It should be the opposite of Module:init/1 and do any
+%% necessary cleaning up. When it returns, the gen_server terminates
+%% with Reason. The return value is ignored.
+%%
+%% @spec terminate(Reason, State) -> void()
+%% @end
+%%--------------------------------------------------------------------
+terminate(_Reason, _State) ->
+    ok.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Convert process state when code is changed
+%%
+%% @spec code_change(OldVsn, State, Extra) -> {ok, NewState}
+%% @end
+%%--------------------------------------------------------------------
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
 
 %%%===================================================================
 %%% Internal functions
